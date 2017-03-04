45e51a6af6de3f0435ed3293bf4b41ba25594a06
Zach Hauser <zacharyhauser@cmail.carleton.ca>
2017-03-04 15:39:06 -0500

    remove search method from view

--- src/View.java
+++ src/View.java
@@ -6,13 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import twitter4j.Paging;
-import twitter4j.Status;
 import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.conf.ConfigurationBuilder;
-
-import java.util.List;
 
 public class View extends Application{
 
@@ -64,22 +58,5 @@ public class View extends Application{
 
     }
 
-    public void search() throws TwitterException {
-
-        System.out.println("THIS IS USERNAME: " + userName);
-
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-        cb.setDebugEnabled(true).setOAuthConsumerKey("ME0BKDlR2hWaSGlc41lm2AGdE");
-        cb.setDebugEnabled(true).setOAuthConsumerSecret("zLuOiqDd37LoAoXLO7EUZgjwDg6zzuR5qrLcyQlGiiztfRwwKA");
-        cb.setDebugEnabled(true).setOAuthAccessToken("780890285727440896-xsL2jycwpbKzw8N1RvPR5O1t04eN2nE");
-        cb.setDebugEnabled(true).setOAuthAccessTokenSecret("TIeQMGQrgguBsFGAbgqRM4BvL4R8dBSIt2E2BaGHapNsm");
-        TwitterFactory tf = new TwitterFactory(cb.build());
-        twitter4j.Twitter twitter = tf.getInstance();
-        Paging page = new Paging(1,100);
-        List<Status> users = twitter.getUserTimeline(userName, page);
-        for (Status user : users) {
-            System.out.println(user + "1");
-        }
-    }
 }
 
