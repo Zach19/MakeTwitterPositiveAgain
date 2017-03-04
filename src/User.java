240131deabf0d98102b442d56812d73b2d799b2f
mikesayegh <michaelsayegh37@gmail.com>
2017-03-04 13:56:25 -0500

    Initial Commit

---
+++ src/User.java
@@ -0,0 +1,11 @@
+/**
+ * Created by mikesayegh on 2017-03-04.
+ */
+public class User {
+    String userName;
+    float score;
+
+    public User(String name){
+        this.userName = name;
+    }
+}
