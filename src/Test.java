d71b8c2c8606aeaf89e3adaca223a5376ff20c49
mikesayegh <michaelsayegh37@gmail.com>
2017-03-04 15:19:00 -0500

    Starting User Class

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,7 @@
-import twitter4j.*;
-import twitter4j.User;
-import twitter4j.auth.BasicAuthorization;
+import twitter4j.Paging;
+import twitter4j.Status;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
 import java.util.List;
@@ -8,14 +9,18 @@ import java.util.List;
 public class Test {
     public static void main(String[] args) throws TwitterException {
         ConfigurationBuilder cb = new ConfigurationBuilder();
+
         cb.setDebugEnabled(true).setOAuthConsumerKey("ME0BKDlR2hWaSGlc41lm2AGdE");
         cb.setDebugEnabled(true).setOAuthConsumerSecret("zLuOiqDd37LoAoXLO7EUZgjwDg6zzuR5qrLcyQlGiiztfRwwKA");
         cb.setDebugEnabled(true).setOAuthAccessToken("780890285727440896-xsL2jycwpbKzw8N1RvPR5O1t04eN2nE");
         cb.setDebugEnabled(true).setOAuthAccessTokenSecret("TIeQMGQrgguBsFGAbgqRM4BvL4R8dBSIt2E2BaGHapNsm");
+
         TwitterFactory tf = new TwitterFactory(cb.build());
         twitter4j.Twitter twitter = tf.getInstance();
         Paging page = new Paging(1,100);
-        List<Status> users = twitter.getUserTimeline("google", page);
+
+        List<Status> users = twitter.getUserTimeline("mike_sayegh", page);
+
         for (Status user : users) {
             System.out.println(user + "1");
         }
--- src/User.java
+++ src/User.java
@@ -1,3 +1,8 @@
+import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
+
+import java.util.List;
+
 /**
  * Created by mikesayegh on 2017-03-04.
  */
@@ -8,4 +13,28 @@ public class User {
     public User(String name){
         this.userName = name;
     }
+
+    public String[] compileTweets() throws TwitterException{
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+
+        cb.setDebugEnabled(true).setOAuthConsumerKey("ME0BKDlR2hWaSGlc41lm2AGdE");
+        cb.setDebugEnabled(true).setOAuthConsumerSecret("zLuOiqDd37LoAoXLO7EUZgjwDg6zzuR5qrLcyQlGiiztfRwwKA");
+        cb.setDebugEnabled(true).setOAuthAccessToken("780890285727440896-xsL2jycwpbKzw8N1RvPR5O1t04eN2nE");
+        cb.setDebugEnabled(true).setOAuthAccessTokenSecret("TIeQMGQrgguBsFGAbgqRM4BvL4R8dBSIt2E2BaGHapNsm");
+
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        twitter4j.Twitter twitter = tf.getInstance();
+        Paging page = new Paging(1,100);
+
+        List<Status> users = twitter.getUserTimeline("mike_sayegh", page);
+
+        for (Status user : users) {
+            System.out.println(user + "1");
+        }
+
+
+
+        return null;
+    }
+
 }
