180234ed6e3ee6a9439df147c3dabf8c92ca8525
GilbertHLam <gilberthlam@cmail.carleton.ca>
2017-03-04 14:56:12 -0500

    Testing the functions

--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,23 @@
-import twitter4j.Status;
-import twitter4j.Twitter;
+import twitter4j.*;
+import twitter4j.User;
+import twitter4j.auth.BasicAuthorization;
+import twitter4j.conf.ConfigurationBuilder;
 
 import java.util.List;
 
 public class Test {
-    public static void main(String[] args){
-        Twitter twitter = new Twitter(twitterID,twitterPassword);
-        List statuses = twitter.getFriendsTimeline();
-        System.out.println("Showing friends timeline.");
-        for(int i=0; i < statuses.size() ; i++) {
-            Status status = (Status)statuses.get(i);
-            System.out.println(status.getUser().getName() + ":" +
-                    status.getText());
+    public static void main(String[] args) throws TwitterException {
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true).setOAuthConsumerKey("ME0BKDlR2hWaSGlc41lm2AGdE");
+        cb.setDebugEnabled(true).setOAuthConsumerSecret("zLuOiqDd37LoAoXLO7EUZgjwDg6zzuR5qrLcyQlGiiztfRwwKA");
+        cb.setDebugEnabled(true).setOAuthAccessToken("780890285727440896-xsL2jycwpbKzw8N1RvPR5O1t04eN2nE");
+        cb.setDebugEnabled(true).setOAuthAccessTokenSecret("TIeQMGQrgguBsFGAbgqRM4BvL4R8dBSIt2E2BaGHapNsm");
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        twitter4j.Twitter twitter = tf.getInstance();
+        Paging page = new Paging(1,100);
+        List<Status> users = twitter.getUserTimeline("google", page);
+        for (Status user : users) {
+            System.out.println(user + "1");
         }
     }
 }
