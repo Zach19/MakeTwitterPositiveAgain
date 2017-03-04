10553277fee7fabac599d2ec3ab47098c9e95330
mikesayegh <michaelsayegh37@gmail.com>
2017-03-04 14:02:37 -0500

    Create Tweet class

---
+++ src/Tweet.java
@@ -0,0 +1,11 @@
+/**
+ * Created by mikesayegh on 2017-03-04.
+ */
+public class Tweet {
+    String content;
+    String date;
+
+    public Tweet(String content){
+        this.content = content;
+    }
+}
