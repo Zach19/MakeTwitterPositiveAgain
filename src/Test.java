da1204054f5ff490faa5d535ea401376b2207a6c
GilbertHLam <gilberthlam@cmail.carleton.ca>
2017-03-04 14:08:23 -0500

    Merge branch 'master' of C:\Users\Gilbert Lam\Documents\GitHub\MakeTwitterPositiveAgain with conflicts.

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,17 @@
+import twitter4j.Status;
+import twitter4j.Twitter;
+
+import java.util.List;
+
 public class Test {
     public static void main(String[] args){
-        System.out.println("gu");
+        Twitter twitter = new Twitter(twitterID,twitterPassword);
+        List statuses = twitter.getFriendsTimeline();
+        System.out.println("Showing friends timeline.");
+        for(int i=0; i < statuses.size() ; i++) {
+            Status status = (Status)statuses.get(i);
+            System.out.println(status.getUser().getName() + ":" +
+                    status.getText());
+        }
     }
 }
